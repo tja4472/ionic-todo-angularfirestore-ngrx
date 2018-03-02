9fd28b9cf62e8c71591756ad0844bacb8f9ddd02
Tim <tja2353@gmail.com>
2018-03-02 11:22:08 +0000

    (app) setStartPage()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,11 +26,17 @@ import * as FromRoot from '../reducers';
 import { AuthService } from './auth/auth.service';
 import { UserService } from './user/user.service';
 
-export interface PageInterface {
+interface PageInterface {
   title: string;
   component: any;
   icon: string;
-  logsOut?: boolean;
+  doSignOut?: boolean;
+}
+
+interface SelectSource {
+  label: string;
+  selected: boolean;
+  value: string;
 }
 
 @Component({
@@ -40,20 +46,15 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   public displayUserName: string;
-  /*
-  public viewTodoLists = [
-    { value: 'aa', label: 'AA' },
-    { value: 'bb', label: 'BB' },
-  ];
-*/
+
   public viewTodoListsSelect$: any;
 
-  appPages: PageInterface[] = [
+  public viewAppPages: PageInterface[] = [
     { title: 'Page One', component: Page1, icon: 'calendar' },
     { title: 'Page Two', component: Page2, icon: 'calendar' },
   ];
 
-  loggedInPages: PageInterface[] = [
+  public viewSignedInPages: PageInterface[] = [
     { title: 'Current Todos', component: TodoListPage, icon: 'calendar' },
     {
       component: TodoCompletedListPage,
@@ -61,10 +62,10 @@ export class MyApp {
       title: 'Completed Todos',
     },
     { title: 'Todo Lists', component: TodoListsPage, icon: 'calendar' },
-    { title: 'Sign Out', component: Page1, icon: 'log-out', logsOut: true },
+    { title: 'Sign Out', component: Page1, icon: 'log-out', doSignOut: true },
   ];
 
-  loggedOutPages: PageInterface[] = [
+  public viewSignedOutPages: PageInterface[] = [
     { title: 'Sign In', component: SignInPage, icon: 'log-in' },
     { title: 'Register', component: RegisterPage, icon: 'person-add' },
   ];
@@ -87,32 +88,87 @@ export class MyApp {
     private userService: UserService,
   ) {
     this.initializeApp();
-    /*
-    this.store
-      .select(FromRoot.getAuthState)
-      .pipe(
-        // Ignore setting of initial state
-        skip(1),
-      )
-      .subscribe((authState) => {
-        console.log('authState>', authState);
+  }
 
-        if (authState.isAuthenticated) {
-          this.enableMenu(true);
-          this.nav.setRoot(TodoListPage).catch(() => {
-            console.error('Did not set nav root');
-          });
+  public viewtodoListsSelectChange(todoListId: any): void {
+    console.log('todoListId>', todoListId);
+    this.userService.SetTodoListId(todoListId);
+  }
 
-          this.displayUserName = authState.displayName;
-        } else {
-          this.displayUserName = 'Not signed in';
-          this.enableMenu(false);
-          this.nav.setRoot(SignInPage).catch(() => {
-            console.error('Did not set nav root');
-          });
-        }
+  initializeApp() {
+    this.setStartPage();
+
+    this.platform.ready().then(() => {
+      console.log('platform.ready()');
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+
+      this.viewTodoListsSelect$ = this.userService.todoListId$().pipe(
+        combineLatest(this.todoListsService.getItems$()),
+        map(([todoListId, todoLists]) => {
+          const ionSelectSource: ReadonlyArray<SelectSource> = todoLists.map((item) => ({
+            label: item.name,
+            selected: item.id === todoListId,
+            value: item.id,
+          }));
+          return ionSelectSource;
+        }),
+      );
+
+      this.userService.todoListId$().subscribe((x) => {
+        console.log('this.userService.todoListId$>', x);
       });
-*/
+    });
+  }
+
+  // Used in view.
+  public isActive(page: PageInterface) {
+    if (
+      this.nav.getActive() &&
+      this.nav.getActive().component === page.component
+    ) {
+      return 'primary';
+    }
+    return;
+  }
+  openPage(page: PageInterface) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    // this.nav.setRoot(page.component);
+    // this.rootPage = page.component;
+
+    if (page.doSignOut === true) {
+      this.authService.signOut();
+    } else {
+      this.rootPage = page.component;
+    }
+  }
+
+  private enableMenu(signedIn: boolean): void {
+    //
+    if (!this.menuController.get(this.signedInMenuId)) {
+      console.error(
+        `%s:enableMenu() *** WARNING: Menu not found>`,
+        this.CLASS_NAME,
+        this.signedInMenuId,
+      );
+    }
+
+    if (!this.menuController.get(this.signedOutMenuId)) {
+      console.error(
+        `%s:enableMenu() *** WARNING: Menu not found>`,
+        this.CLASS_NAME,
+        this.signedOutMenuId,
+      );
+    }
+
+    this.menuController.enable(signedIn, this.signedInMenuId);
+    this.menuController.enable(!signedIn, this.signedOutMenuId);
+  }
+
+  private setStartPage(): void {
+    //
     this.store
       .select(FromRoot.getAuthState)
       .pipe(
@@ -147,129 +203,5 @@ export class MyApp {
 
         this.displayUserName = authState.displayName;
       });
-    /*
-    this.store
-      .select(FromRoot.getAuthState)
-      .pipe(
-        combineLatest(this.store.select(FromRoot.getUserState)),
-        // Ignore setting of initial state
-        // skip(1),
-        filter(([, userState]) => userState.todoListId !== ''),
-        //         filter(([,userState]) => userState.isLoaded == true),
-      )
-      .subscribe(([authState, userState]) => {
-        console.log('########authState>', authState);
-        console.log('########userState>', userState);
-
-        if (authState.isAuthenticated) {
-          this.enableMenu(true);
-          this.nav.setRoot(TodoListPage).catch(() => {
-            console.error('Did not set nav root');
-          });
-
-          this.displayUserName = authState.displayName;
-        } else {
-          this.displayUserName = 'Not signed in';
-          this.enableMenu(false);
-          this.nav.setRoot(SignInPage).catch(() => {
-            console.error('Did not set nav root');
-          });
-        }
-      });
-*/
-  }
-
-  public viewtodoListsSelectChange(todoListId: any): void {
-    console.log('todoListId>', todoListId);
-    this.userService.SetTodoListId(todoListId);
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      console.log('platform.ready()');
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-
-      this.viewTodoListsSelect$ = this.userService.todoListId$().pipe(
-        combineLatest(this.todoListsService.getItems$()),
-        map(([todoListId, todoLists]) => {
-          const ionSelectSource = todoLists.map((item) => ({
-            label: item.name,
-            selected: item.id === todoListId,
-            value: item.id,
-          }));
-          return ionSelectSource;
-        }),
-      );
-
-      this.userService.todoListId$().subscribe((x) => {
-        console.log('this.userService.todoListId$>', x);
-      });
-    });
-  }
-
-  // Used in view.
-  public isActive(page: PageInterface) {
-    if (
-      this.nav.getActive() &&
-      this.nav.getActive().component === page.component
-    ) {
-      return 'primary';
-    }
-    return;
-  }
-  openPage(page: PageInterface) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    // this.nav.setRoot(page.component);
-    // this.rootPage = page.component;
-
-    if (page.logsOut === true) {
-      this.authService.signOut();
-    } else {
-      this.rootPage = page.component;
-    }
-  }
-  /*
-  private doSignedIn(user: SignedInUser): void {
-    // this.rootPage = TodoListPage;
-    this.enableMenu(true);
-    this.nav.setRoot(TodoListPage).catch(() => {
-      console.error('Did not set nav root');
-    });
-
-    this.displayUserName = user.displayName;
-  }
-
-  private doSignedOut(): void {
-    // this.rootPage = SignInPage;
-    this.displayUserName = 'Not signed in';
-    this.enableMenu(false);
-    this.nav.setRoot(SignInPage).catch(() => {
-      console.error('Did not set nav root');
-    });
-  }
-*/
-  private enableMenu(signedIn: boolean): void {
-    //
-    if (!this.menuController.get(this.signedInMenuId)) {
-      console.error(
-        `%s:enableMenu() *** WARNING: Menu not found>`,
-        this.CLASS_NAME,
-        this.signedInMenuId,
-      );
-    }
-
-    if (!this.menuController.get(this.signedOutMenuId)) {
-      console.error(
-        `%s:enableMenu() *** WARNING: Menu not found>`,
-        this.CLASS_NAME,
-        this.signedOutMenuId,
-      );
-    }
-
-    this.menuController.enable(signedIn, this.signedInMenuId);
-    this.menuController.enable(!signedIn, this.signedOutMenuId);
   }
 }
